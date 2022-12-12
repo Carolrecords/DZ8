6df7bd5dc32dd97122b7a42cd40402d521ddb890
korolev_as <antonykas@mail.ru>
2022-12-13 00:56:02 +0300

    init

---
+++ ZaDacha4/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+//66(0,0,0) 25(0,1,0)
+//34(1,0,0) 41(1,1,0)
+//27(0,0,1) 90(0,1,1)
+//26(1,0,1) 55(1,1,1)
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество глубины массива: ");
+int depth = int.Parse(Console.ReadLine() ?? "");
+
+int[,,] array = GetArray(rows, columns, depth, 10, 100);
+PrintArray(array);
+
+//=======================================================
+
+int[,,] GetArray(int r, int c, int d, int minValue, int maxValue)
+{
+    int[,,] result = new int[r, c, d];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            for (int k = 0; k < d; k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ZaDacha5/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+//11 16 15 06
+//10 09 08 07
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = new int[rows, columns];
+
+SpiralArray(0, 0);
+PrintArray(array);
+
+//=======================================================
+
+void SpiralArray(int row, int col)
+{
+    int index = 1;
+    int reduction = 0; // переменная, которая позволяет за цикл обхода прямоугольник, уменьшить последующий прямоугольник
+
+    while (index <= rows * columns)
+    {
+        array[row, col] = index;
+        if (row == reduction && col < columns - reduction - 1)
+            col++;
+        else if (col == columns - reduction - 1 && row < rows - reduction - 1)
+            row++;
+        else if (row == rows - reduction - 1 && col > reduction)
+            col--;
+        else
+            row--;
+
+        if (row == reduction + 1 && col == reduction && reduction != columns - reduction - 1)
+        {
+            reduction++;
+        }
+        index++;
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+                Console.Write($"0{inArray[i, j]} ");
+
+            else
+                Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
